d055ee32568464345f07d7993d2485a5347b7666
Mark Tinker <mark.tinker.94@gmail.com>
2025-07-18 11:17:37 -0400

    Works on sentance and dictionary but is case sensitive

--- substring.rb
+++ substring.rb
@@ -2,14 +2,18 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(word, dict)
   words = word.split(" ")
+  p words
   dict.reduce(Hash.new(0)) do |result, word_in_dict|
+    words.each do |word|
       if word.include?(word_in_dict)
         result[word_in_dict] += 1
         result
       else
         result
       end
+    end
+    result
   end
 end
 
-p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
