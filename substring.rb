d38966f89101e5e0939a142a5ed6cacfa2307c40
Mark Tinker <mark.tinker.94@gmail.com>
2025-07-18 11:19:38 -0400

    now works regardless of case

--- substring.rb
+++ substring.rb
@@ -1,11 +1,11 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, dict)
-  words = word.split(" ")
+  words = word.downcase.split(" ")
   p words
   dict.reduce(Hash.new(0)) do |result, word_in_dict|
     words.each do |word|
-      if word.include?(word_in_dict)
+      if word.include?(word_in_dict.downcase)
         result[word_in_dict] += 1
         result
       else
